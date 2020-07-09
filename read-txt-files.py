99e3af2d04154a1bba7048d4e12b3ec407dd37b3
Ahmet Nuri Ozcelik <ahmetnuri.ozcelik@outlook.com>
2020-07-09 17:38:05 -0400

    Folder updated 2020-07-09

--- read-txt-files.py
+++ read-txt-files.py
@@ -1,10 +1,16 @@
 # The file is opened, read and closed
 handle= open("quotes.txt", "r")
 file_read=handle.read()
-print(file_read)
+print('This is  the open/close method \n',file_read)
 handle.close()
 
 # The file is closed automatically by using with statement
 with open("quotes.txt", "r") as handle:
     file_read=handle.read()
-    print(file_read)
+    print('This is the with option \n',file_read)
+
+# For loop is used to read the file
+with open("quotes.txt", "r") as handle:
+    print("File is read using a for loop line by line")
+    for line in handle:
+        print (line.strip())
