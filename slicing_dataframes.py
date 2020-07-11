247f7c5f2caf534a9828c74556884d27179fb99c
Ahmet Nuri Ozcelik <ahmetnuri.ozcelik@outlook.com>
2020-07-10 20:56:09 -0400

    Update Folder 2020-07-10

---
+++ slicing_dataframes.py
@@ -0,0 +1,9 @@
+import pandas as pd
+covid_all=pd.read_csv("owid-covid-data.csv")
+#Slices the file with the selected columns below
+covid_sliced = covid_all[["location","new_cases_per_million","date"]]
+#Slices the file with condition location=Canada
+condition=covid_sliced['location']=="Canada"
+covid_canada=covid_sliced[condition]
+#writes the file to a csv file 
+covid_canada.to_csv('covid-canada.csv')
